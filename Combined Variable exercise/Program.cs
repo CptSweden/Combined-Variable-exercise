547f11cd42ad6f79b269c82a3a711dfd666e0d05
joelb <joelb@DESKTOP-P8KJ30R>
2025-08-30 15:04:32 +0200

    Add project files.

---
+++ Combined Variable exercise/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Combined_Variable_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string firstName = "Joel";
+
+            string lastName = "Boqvist";
+
+            int age = 31;
+
+            Console.WriteLine("My name is " + firstName + " " + lastName + " and I'm " + age + " years old.");
+            */
+
+
+
+            /*
+            string firstName = "Joel";
+
+            double height = 1.72;
+
+            Console.WriteLine(firstName + " is " + height + " meters tall.");
+            */
+
+
+
+            /*
+            string firstName = "Joel";
+
+            bool likesPizza = true;
+
+            Console.WriteLine("Does " + firstName + " like pizza? " + likesPizza);
+            */
+
+
+
+            /*
+            string firstName = "Joel";
+
+            string lastName = "Boqvist";
+
+            string hobby = "music";
+
+            int age = 31;
+
+            bool isStudent = true;
+
+            Console.WriteLine("My name is " + firstName + " " + lastName + ", I'm " + age + " years old, my hobby is " + hobby + " and it's " + isStudent + " that I'm a student.");
+            */
+
+
+
+
+            string firstName = "Joel";
+
+            char grade = 'A';
+
+            Console.WriteLine(firstName + "´s grade is " + grade);
+
+
+
+
+
+
+        }
+    }
+}
